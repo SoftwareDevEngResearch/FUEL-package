dc98420d56e40927b81928e382b5c5c1c30c43e6
HeatherMM1321 <millerh@oregonstate.edu>
2020-05-21 10:01:28 -0700

    changed some of the formatting, removed the plot cooking eventsfunction (now part of plot_stove function), improved documentation.

--- FUEL/household.py
+++ FUEL/household.py
@@ -5,7 +5,7 @@ from scipy.signal import find_peaks
 
 class Household:
 
-    def __init__(self, dataframe, stoves, fuels, temp_threshold=15,  time_between_events=30):
+    def __init__(self, dataframe, stoves, fuels, temp_threshold=15,  time_between_events=30, weight_threshold=0.2):
         '''When called this class will verify that the input arguments are in the correct formats and set self values
 
         Args:
@@ -23,14 +23,17 @@ class Household:
 
             time_between_events (int): The time threshold (minutes) that will mark the minimum time between cooking
                                        events. This value should be greater than or equal to zero.
+            weight_threshold (float): The weight change (kg) that should be ignored. All weight changes above this
+                                      value will be marked. Defaults to 0.2 kg.
 
-        Returns:
+        Returns:'[[
             df_stoves : Input dataframe
             stoves : Input stoves
             fuels : Input fuels
             temp_threshold: Input temperature threshold
             time_between_events: Input time between cooking events
             study_duration: The duration of the study in datetime format
+            self.weight_threshold: Input weight threshold
 
         '''
 
@@ -63,6 +66,7 @@ class Household:
         self.temp_threshold = temp_threshold
         self.time_between_events = time_between_events
         self.study_duration = self.df_stoves['timestamp'].iloc[-1]-self.df_stoves['timestamp'][0]
+        self.weight_threshold = weight_threshold
 
     def check_stove_type(self, stove="All"):
         '''Check if stove input is in dataset
@@ -101,7 +105,7 @@ class Household:
         '''Check if fuel input is in dataset
 
         Args:
-            fuel (string): The name of the stove(s) that is being checked for. If multiple fuels are desired they must
+            fuel (string): The name of the fuel(s) that is being checked for. If multiple fuels are desired they must
                             be input as a list of stings.
 
         Returns:
@@ -114,8 +118,6 @@ class Household:
             for f in fuel:
                 if type(f) != str:
                     raise ValueError('Must input all fuels as strings!')
-                else:
-                    raise ValueError(f+' not found in data set.')
         elif type(fuel) != str:
             raise ValueError('Must input fuel type as string!')
 
@@ -127,68 +129,8 @@ class Household:
                 if f in self.fuels:
                     fuel_type.append(f)
             if not fuel_type:
-                raise ValueError('Fuel not found in data set.')
+                raise ValueError(f +' not found in data set.')
         return fuel_type
-
-    def plot_stove(self, stove="All"):
-        '''Plotting the temperature data of stoves over duration of study.
-
-        Args:
-            stove (str): If only plotting one stove temperature readings stove must be input as a str. If plotting
-                         multiple stoves, stoves must be input as a list of stoves. Defaults to plotting all stoves
-                         in data set.
-
-        Returns:
-              figure : Returns interactive line plots of all requested stove temperature readings over the
-                       duration of the study. To show the figure .show() must be added after calling the function.
-        '''
-
-        stove_type = self.check_stove_type(stove)
-
-        fig = go.Figure()
-
-        fig.update_yaxes(title_text="Temp")
-        fig.update_xaxes(title_text="Time")
-        fig.update_layout(title_text=stove + " Stove Temperature")
-
-        for s in stove_type:
-            fig.add_trace(go.Scatter(
-                        x=self.df_stoves['timestamp'],
-                        y=self.df_stoves[s].values,
-                        mode='lines',
-                        name=s.split(' ')[0],
-                        ))
-        return fig
-
-    def plot_fuel(self, fuel="All"):
-        '''Plotting the fuel weight data over duration of study.
-
-        Args:
-            fuel (str): If only plotting one fuel weight data fuel must be input as a string. If plotting
-                         multiple fuels, fuels must be input as a list of fuels. Defaults to plotting all fuels
-                         in data set.
-
-        Returns:
-            figure : Returns interactive line plots of all requested fuel weight data over the
-                       duration of the study. To show the figure .show() must be added after calling the function.
-        '''
-
-        fuel_type = self.check_fuel_type(fuel)
-
-        fig = go.Figure()
-
-        fig.update_yaxes(title_text="Weight")
-        fig.update_xaxes(title_text="Time")
-        fig.update_layout(title_text=fuel + " Weight Readings")
-
-        for f in fuel_type:
-            fig.add_trace(go.Scatter(
-                            x=self.df_stoves['timestamp'],
-                            y=self.df_stoves[f].values,
-                            mode='lines',
-                            name=f.split(' ')[0],
-                            ))
-        return fig
 
     def cooking_events(self, stove="All"):
         ''' Determine the number of cooking events on each stove during study.
@@ -207,7 +149,6 @@ class Household:
 
         '''
 
-
         if type(stove) != str:
             raise ValueError('Must input fuel type as a string!')
 
@@ -223,51 +164,41 @@ class Household:
             cook_events_list.update({s: peaks})
 
         self.cook_events = cook_events_list
-
         return number_of_cooking_events
 
-    def plot_cooking_events(self, stove="All"):
-        '''Must add .show() after calling function for plot to be generated'''
+    def fuel_usage(self, fuel="All"):
+        '''Determine the total amount of each fuel used on each day of the study.
 
-        # The temperature threshold must an integer >= 0
-        # The time between cooking events must be an integer >= 0
-        # The stove type must be entered as a string and must be in the data set
-        # If the temperature threshold and time between events is not specified it will revert back to default value even
-        # if it was set to different values when calling cooking_events() function.
+        Args:
+            fuel (string): The name of the fuel(s) that is being checked for. If multiple fuels are desired they must
+                            be input as a list of stings.
+        Returns:
+            self.weight_changes (dict): A dictionary of all significant fuel changes in study. Key is the fuel type,
+                                        values are lists of indices where significant fuel changes took place. This is
+                                        used by another function internally.
+            Daily fuel usage (dataframe): A dataframe, rows = fuel types, columns = day (24hr period) of study,
+                                          values are the amount of fuel (kg) used in that day of study.
+        '''
 
-        if type(stove) != str:
-            raise ValueError('Must input fuel type as a string!')
-
-        stove_type = self.check_stove_type(stove)
-
-        self.cooking_events(stove)
-        events = self.cook_events
-        fig = self.plot_stove(stove)
-
-        for s in stove_type:
-            fig.add_trace(
-                go.Scatter(x=self.df_stoves['timestamp'][events[s]],
-                           y=self.df_stoves[s][events[s]],
-                           mode='markers',
-                           name=s + ' Cooking Events'
-                           )
-                        )
-        return fig
-
-    def fuel_usage(self, fuel = "All", weight_threshold=0.1):
-        '''Returns the total of each fuel used on each day of the study.'''
-
-        # Should input which stoves you are interested in and the weight threshold
-        # weight threshold is the weight (kg) change that yu would like to ignore
-        # returns a dataframe with all of the selected fuels and how much of each fuel was used each day.
-
-        if type(fuel) is not str:
-            raise ValueError('Only one fuel may be put in at a time and it must be entered as a string.')
+        if type(fuel) == list:
+            for f in fuel:
+                if type(f) != str:
+                    raise ValueError('Must input all fuels as strings!')
+        elif type(fuel) != str:
+            raise ValueError('Must input fuel type as string!')
 
         fuel_type = self.check_fuel_type(fuel)
 
         def find_significant_changes(peaks):
-            '''This function finds all changes in weight that exceed the threshold. '''
+            '''Find all significant weight changes.
+
+             Args:
+                 peaks (list): A list of all fuel change indices found in the data set.
+
+             Returns:
+                 weight_change (list): A list of all fuel change indices found that resulted in a change of fuel weight
+                                       larger than the prescribed threshold (weight_threshold).
+                 '''
 
             weight = self.df_stoves[f][peaks[0]]
             weight_change = [peaks[0]]
@@ -275,7 +206,7 @@ class Household:
             # if the weight difference between these peaks is less than the weight threshold ignore it
             for i in peaks[1:]:
                 new_weight = self.df_stoves[f][i]
-                if abs(new_weight - weight) < weight_threshold:
+                if abs(new_weight - weight) < self.weight_threshold:
                     pass
                 else:
                     weight_change.append(i)
@@ -291,7 +222,17 @@ class Household:
             return weight_change
 
         def daily_fuel_use(fuel, weight_changes):
-            '''Determines How much of a fuel was used in each day of the study.'''
+            '''Determine amount of fuel used in each 24hr period of study.
+
+            Args:
+                fuel (str): The name of a fuel.
+                weight_changes (list): List of indices of all significant fuel changes for chosen fuel in study.
+
+            Returns:
+                daily_fuel_usage (dict): A dictionary containing fuel usage information for each day of study. Keys
+                                         represent the day of the study, values is the total weight change (kg) recorded
+                                         for that day of the study.
+            '''
 
             daily_fuel_usage = {}
             day = 1
@@ -321,27 +262,45 @@ class Household:
             return daily_fuel_usage
 
         fuel_change = []
-
+        fuel_weight_changes = {}  # will be used in other functions
         for f in fuel_type:
             peaks = find_peaks(self.df_stoves[f].values, height=1, distance=1)[0]
             weight_changes = find_significant_changes(peaks)
+            fuel_weight_changes.update({f: weight_changes})
             daily_usage = daily_fuel_use(f, weight_changes)
             fuel_change.append(daily_usage)
 
+        self.weight_changes = fuel_weight_changes
         return pd.DataFrame(fuel_change, index=fuel_type)
 
     def cooking_duration(self, stove="All"):
-        '''This will return a data frame with the number of cooking minutes for each day for each stove.'''
+        '''Determines the cooking duration (mins) on each stove for each day of the study.
 
-        # First the total cooking duration for each event on each stove will be determined.
-        # Then the total cooking duration for each day on each stove will be determined
-        # Finally a data frame will be produced that contains this information.
+        Args:
+            stove (str): If only looking at one stove, stove must be input as a str. If looking at
+                         multiple stoves, stoves must be input as a list of stoves. Defaults all stoves
+                         in data set.
+        Returns:
+            Cooking Durations (dataframe):  A dataframe, rows = stove type, columns = day of study, value duration
+                                            of cooking (mins) on a stove during that day of the study.
 
-        def cooking_durations(cooking_events_index, cooking_temps):
-            ''' This will return a list of the beginning and end time for every identified cooking event on a stove.'''
+        '''
 
-            # Takes in the index of each identified cooking event and the recorded temperature for that stove
-            # Finds the beginning and end of cooking event by finding the lowest temperatures on each side of the event.
+        def cooking_durations(stove):
+            ''' Determine the indices of the start and end of each cooking event.
+
+            Args:
+               stove (str): Name of a cook stove in data set.
+
+            Returns:
+                cooking_events_list (list) : A list of tuples containing the indices of the beginning and end of a
+                                             cooking event [(cooking begins, cooking ends)].
+
+            '''
+
+            cooking_events_index = self.cook_events[stove]
+            cooking_temps = self.df_stoves[stove]
+
             cooking_event_list = []
 
             for i in cooking_events_index:
@@ -363,12 +322,18 @@ class Household:
             return cooking_event_list
 
         def daily_cooking_time(cooking_durations_list):
-            '''This will return the total cooking time on a stove for each 24 hour period of study.'''
+            '''Determine the total time spent cooking on a stove (mins) for each day of the study.
 
-            # Takes in the list of cooking event durations
-            # Determines the length of each cooking event and which day of the study it occurred on.
-            # Adds the duration of cooking time to a dictionary associated with the correct day.
-            # If no cooking was recorded on a stove during a day it will add a 0 value entry to dictionary for that day.
+            Args:
+                cooking_duration_list (list): A list of tuples containing the indices of the beginning and end of a
+                                             cooking event [(cooking begins, cooking ends)].
+
+            Returns:
+                 daily_cooking (dict): A dictionary containing stove cooking information for each day of study. Keys
+                                         represent the day of the study, values are the total time(min) cooking recorded
+                                         for that day of the study.
+
+            '''
 
             day = 0
             daily_cooking = {}
@@ -404,16 +369,106 @@ class Household:
         all_cooking_info = []
 
         for s in stoves:
-            cooking_events_index = self.cook_events[s]
-            cooking_temps = self.df_stoves[s]
-
-            cooking_durations_list = cooking_durations(cooking_events_index, cooking_temps)
+            cooking_durations_list = cooking_durations(s)
             daily_cooking = daily_cooking_time(cooking_durations_list)
 
             all_cooking_info.append(daily_cooking)
 
         return pd.DataFrame(all_cooking_info, index=stoves)
 
+    def plot_stove(self, stove="All", cooking_events=False):
+        '''Plotting the temperature data of stoves over duration of study.
+
+        Args:
+            stove (str): If only plotting one stove temperature readings stove must be input as a str. If plotting
+                         multiple stoves, stoves must be input as a list of stoves. Defaults to plotting all stoves
+                         in data set.
+            cooking_events (bool): If it is desired to have cooking events marked cooking_events must be set to
+                                   True. Default is False and will only show the cook stove temperature data with no
+                                   cooking events marked.
+
+        Returns:
+              figure : Returns interactive line plots of all requested stove temperature readings over the
+                       duration of the study. To show the figure .show() must be added after calling the function. If
+                       cooking_events = True plot will show determined cooking events with a point.
+        '''
+
+        stove_type = self.check_stove_type(stove)
+
+        fig = go.Figure()
+
+        fig.update_yaxes(title_text="Temp")
+        fig.update_xaxes(title_text="Time")
+        fig.update_layout(title_text=stove + " Stove Temperature")
+
+        for s in stove_type:
+            fig.add_trace(go.Scatter(
+                        x=self.df_stoves['timestamp'],
+                        y=self.df_stoves[s].values,
+                        mode='lines',
+                        name=s.split(' ')[0],
+                        ))
+
+        if cooking_events:
+            self.cooking_events(stove)
+            events = self.cook_events
+
+            for s in stove_type:
+                fig.add_trace(
+                go.Scatter(x=self.df_stoves['timestamp'][events[s]],
+                           y=self.df_stoves[s][events[s]],
+                           mode='markers',
+                           name=s + ' Cooking Events'
+                           )
+                        )
+        return fig
+
+    def plot_fuel(self, fuel="All", fuel_usage=False):
+        '''Plotting the fuel weight data over duration of study.
+
+        Args:
+            fuel (str): If only plotting one fuel weight data fuel must be input as a string. If plotting
+                         multiple fuels, fuels must be input as a list of fuels. Defaults to plotting all fuels
+                         in data set.
+            fuel_usage (bool): If it is desired to have fuel usage marked fuel_usage must be set to True.
+                               Default is False and will only show the fuel weight data with no weight changes marked.
+
+        Returns:
+            figure : Returns interactive line plots of all requested fuel weight data over the
+                       duration of the study. To show the figure .show() must be added after calling the function.
+                       If fuel_usage=True all significant fuel weight changes will be marked on the plot.
+        '''
+
+        fuel_type = self.check_fuel_type(fuel)
+
+        fig = go.Figure()
+
+        fig.update_yaxes(title_text="Weight")
+        fig.update_xaxes(title_text="Time")
+        fig.update_layout(title_text=fuel + " Weight Readings")
+
+        for f in fuel_type:
+            fig.add_trace(
+                go.Scatter(
+                            x=self.df_stoves['timestamp'],
+                            y=self.df_stoves[f].values,
+                            mode='lines',
+                            name=f.split(' ')[0],
+                            ))
+
+        if fuel_usage:
+            self.fuel_usage(fuel=fuel_type)
+            changes = self.weight_changes
+
+            for f in fuel_type:
+                fig.add_trace(
+                       go.Scatter(x=self.df_stoves['timestamp'][changes[f]],
+                           y=self.df_stoves[f][changes[f]],
+                           mode='markers',
+                           name=f + ' Weight Change'
+                           )
+                        )
+        return fig
 
 if __name__ == "__main__":
 
@@ -424,5 +479,7 @@ if __name__ == "__main__":
 
     x = Household(df, stoves, fuels)
 
-    print(x.check_fuel_type(['firewod', 'lpg']))
+
+    x.plot_fuel(fuel_usage=True).show()
+
 
