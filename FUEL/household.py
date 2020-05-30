319ce44e163421578e2c77ca94a127384b9049f9
HeatherMM1321 <millerh@oregonstate.edu>
2020-05-29 17:16:48 -0700

    fixed the fuel usage function but I still need to double check some of the lpg functionality

--- FUEL/household.py
+++ FUEL/household.py
@@ -5,7 +5,7 @@ from scipy.signal import find_peaks
 
 class Household:
 
-    def __init__(self, dataframe, stoves, fuels, temp_threshold=15, time_between_events=30, weight_threshold=0.2):
+    def __init__(self, dataframe, stoves, fuels, hh_id, temp_threshold=15, time_between_events=30, weight_threshold=0.2):
         '''Verifying that the input arguments are in the correct formats and set self values
 
         Args:
@@ -17,6 +17,8 @@ class Household:
 
             fuels (list): Fuels in the study (these should match the names of the column headers for the
                           fuel information in the dataframe exactly).
+
+            hh_id (str): The individual household identification
 
             temp_threshold (int): A temperature threshold (degrees) for which all cooking events are identified.
                                   This value should be greater than or equal to zero. Defaults to 15.
@@ -31,6 +33,7 @@ class Household:
             df_stoves : Input dataframe
             stoves : Input stoves
             fuels : Input fuels
+            hh_id : Input Household ID
             temp_threshold: Input temperature threshold
             time_between_events: Input time between cooking events
             study_duration: The duration of the study in datetime format
@@ -64,6 +67,7 @@ class Household:
         self.df_stoves = dataframe.applymap(lambda i: i.lower() if type(i) == str else i)
         self.stoves = [i.lower() for i in stoves]
         self.fuels = [i.lower() for i in fuels]
+        self.hh_id = hh_id
         self.temp_threshold = temp_threshold
         self.time_between_events = time_between_events
         self.study_duration = self.df_stoves['timestamp'].iloc[-1] - self.df_stoves['timestamp'][0]
@@ -167,40 +171,96 @@ class Household:
 
         self.cook_events = cook_events_list
         return number_of_cooking_events
+    #
+    # def _find_significant_weight_changes(self, fuel, peaks):
+    #     '''Find all significant weight changes (internal function).
+    #
+    #     Args:
+    #         fuel (str) : name of fuel in data set
+    #
+    #         peaks (list): A list of all fuel change indices found in the data set.
+    #
+    #     Returns:
+    #         weight_change (list): A list of all fuel change indices found that resulted in a change of fuel weight
+    #                                    larger than the prescribed threshold (weight_threshold).
+    #              '''
+    #     fuel_data = list(self.df_stoves[fuel])
+    #
+    #     weight = fuel_data[peaks[0]]
+    #     weight_change = [peaks[0]]
+    #
+    #     # if the weight difference between these peaks is less than the weight threshold ignore it
+    #     for i in peaks[1:]:
+    #         new_weight = fuel_data[i]
+    #         weight_before = fuel_data[i-1]
+    #         weight_after = fuel_data[i+1]
+    #         weight_diff = new_weight - weight
+    #
+    #         if abs(weight_diff) < self.weight_threshold:
+    #             pass
+    #
+    #         else:
+    #             # check to make sure it isnt catching random peaks
+    #             if weight_diff > self.weight_threshold:
+    #                 if abs(weight_after-weight_before) < self.weight_threshold or weight_after < weight_before:
+    #                     pass
+    #                 else:
+    #                     weight_change.append(i)
+    #                     weight = fuel_data[i]
+    #             else:
+    #                 weight_change.append(i)
+    #                 weight = fuel_data[i]
+    #
+    #         # to make sure that the lowest value is captured check the final weight value against the previous
+    #         if i == peaks[-1]:
+    #             last_idx = len(fuel_data) - 1
+    #             last_weight = fuel_data[last_idx]
+    #             if last_weight < weight:
+    #                 weight_change.append(last_idx)
+    #     return weight_change
 
-    def _find_significant_weight_changes(self, fuel, peaks):
+    def _find_weight_changes(self, fuel):
         '''Find all significant weight changes (internal function).
 
         Args:
             fuel (str) : name of fuel in data set
 
-            peaks (list): A list of all fuel change indices found in the data set.
-
         Returns:
             weight_change (list): A list of all fuel change indices found that resulted in a change of fuel weight
                                        larger than the prescribed threshold (weight_threshold).
-                 '''
-        fuel_data = self.df_stoves[fuel]
+        '''
 
-        weight = fuel_data[peaks[0]]
-        weight_change = [peaks[0]]
+        fuel_data = list(self.df_stoves[fuel])
 
-        # if the weight difference between these peaks is less than the weight threshold ignore it
-        for i in peaks[1:]:
-            new_weight = fuel_data[i]
-            if abs(new_weight - weight) < self.weight_threshold:
+        weight = fuel_data[0]
+        weight_changes = []
+
+        for i, current_weight in enumerate(fuel_data[1:]):
+            if fuel == "lpg" and current_weight < 2: # should change this to be more versatile later
                 pass
             else:
-                weight_change.append(i)
-                weight = fuel_data[i]
+                if i == len(fuel_data)-2:
+                    if fuel_data[i] < weight:
+                        weight_changes.append(i+1)
+                else:
+                    weight_before = fuel_data[i]
+                    weight_after = fuel_data[i+2]
+                    weight_diff = current_weight - weight
 
-            # to make sure that the lowest value is captured check the final weight value against the previous
-            if i == peaks[-1]:
-                last_idx = len(fuel_data) - 1
-                last_weight = fuel_data[last_idx]
-                if last_weight < weight:
-                    weight_change.append(last_idx)
-        return weight_change
+                    if abs(weight_diff) < self.weight_threshold:
+                        pass
+                    else:
+                        # check to make sure it isnt catching random peaks
+                        if weight_diff > self.weight_threshold:
+                            if abs(weight_after-weight_before) < self.weight_threshold or weight_after < weight_before:
+                                pass
+                            else:
+                                weight_changes.append(i+1)
+                                weight = current_weight
+                        else:
+                            weight_changes.append(i+1)
+                            weight = current_weight
+        return weight_changes
 
     def _daily_fuel_use(self, fuel, weight_changes):
         '''Determine amount of fuel used in each 24hr period of study (Internal function).
@@ -222,31 +282,38 @@ class Household:
         study_began = self.df_stoves['timestamp'][0]
         study_duration = round(self.study_duration.total_seconds()/86400) # rounding to the nearest day
         weight = fuel_info[weight_changes[0]]
-
+        weight_diff = 0
         for i in weight_changes[1:]:
-            weight_diff = 0
+
             day_of_use = (self.df_stoves['timestamp'][i] - study_began).days
 
-            if day_of_use == study_duration:
-                new_weight = fuel_info[i]
-                weight_diff += weight - new_weight
-                weight = new_weight
+            new_weight = fuel_info[i]
 
-                daily_fuel_usage.update({day_of_use: weight_diff})
+            if weight - new_weight < 0:
+                # indicates an adding of fuel not a fuel usage
+                pass
 
-            elif day_of_use == day:
-                new_weight = fuel_info[i]
-                weight_diff += weight - new_weight
-                weight = new_weight
-
-                daily_fuel_usage.update({day+1: weight_diff})
             else:
-                weight_diff = 0
-                day = day_of_use
-                new_weight = fuel_info[i]
-                weight_diff += weight - new_weight
 
-                daily_fuel_usage.update({day+1: weight_diff})
+                if day_of_use == study_duration:
+                    # fuel used on final day of study
+                    if weight - new_weight < self.weight_threshold:
+                        weight_diff = 0
+                    else:
+                        weight_diff = weight - new_weight
+                    daily_fuel_usage.update({day_of_use: weight_diff})
+
+                elif day_of_use == day:
+                    weight_diff += weight - new_weight
+                    daily_fuel_usage.update({day+1: weight_diff})
+
+                else:
+                    weight_diff = weight - new_weight
+                    day = day_of_use
+
+                    daily_fuel_usage.update({day+1: weight_diff})
+
+            weight = new_weight
 
         if len(daily_fuel_usage) != study_duration:
             for i in range(study_duration):
@@ -257,13 +324,15 @@ class Household:
 
         return daily_fuel_usage
 
-    def fuel_usage(self, fuel="All"):
+    def fuel_usage(self, fuel="All", min_fuel=2):
         '''Determine the total amount of each fuel used on each day of the study.
 
         Args:
             fuel (string): If only looking at one fuel, fuel must be input as a str. If looking at
                          multiple fuels, fuels must be input as a list of fuels. Defaults all fuels
                          in data set.
+
+            min_fuel(int): The minimum weight(kg) that you want to have weight be recognized
         Returns:
             self.weight_changes (dict): A dictionary of all significant fuel changes in study. Key is the fuel type,
                                         values are lists of indices where significant fuel changes took place. This is
@@ -278,10 +347,11 @@ class Household:
         fuel_change = []
         fuel_weight_changes = {}  # will be used in other functions
         for f in fuel_type:
-            peaks = find_peaks(self.df_stoves[f].values, height=1, distance=1)[0]
-            weight_changes = self._find_significant_weight_changes(f, peaks)
-            fuel_weight_changes.update({f: weight_changes})
-            daily_usage = self._daily_fuel_use(f, weight_changes)
+            # peaks = find_peaks(self.df_stoves[f].values, height=min_fuel, distance=1)[0]
+            # weight_changes = self._find_significant_weight_changes(f, peaks)
+            changes = self._find_weight_changes(f)
+            fuel_weight_changes.update({f: changes})
+            daily_usage = self._daily_fuel_use(f, changes)
             fuel_change.append(daily_usage)
 
         self.weight_changes = fuel_weight_changes
@@ -421,7 +491,8 @@ class Household:
 
         fig.update_yaxes(title_text="Temp")
         fig.update_xaxes(title_text="Time")
-        fig.update_layout(title_text=stove + " Stove Temperature")
+        fig.update_layout(title_text="Household: " + hh_id + " " + stove + " Stove Temperature")
+
 
         for s in stove_type:
             fig.add_trace(go.Scatter(
@@ -467,7 +538,7 @@ class Household:
 
         fig.update_yaxes(title_text="Weight")
         fig.update_xaxes(title_text="Time")
-        fig.update_layout(title_text=fuel + " Weight Readings")
+        fig.update_layout(title_text="Household: " + hh_id + " " + fuel + " Weight Readings")
 
         for f in fuel_type:
             fig.add_trace(
@@ -496,20 +567,32 @@ class Household:
 if __name__ == "__main__":
     from olivier_file_convert import reformat_olivier_files as reformat
 
-    # example file
-    df, stoves, fuels = reformat('./data_files/HH_319_2018-08-25_19-27-32_processed_v2.csv')
+    filepaths = ['HH_38_2018-08-26_15-01-40_processed_v3.csv', # look at the lpg
+             'HH_44_2018-08-17_13-49-22_processed_v2.csv',
+             'HH_141_2018-08-17_17-50-31_processed_v2.csv', #lpg is off here too
+             'HH_318_2018-08-25_18-35-07_processed_v2.csv',
+             'HH_319_2018-08-25_19-27-32_processed_v2.csv',
+             'HH_326_2018-08-25_17-52-16_processed_v2.csv',
+             'HH_345_2018-08-25_15-52-57_processed_v2.csv',
+             'HH_371_2018-08-17_15-31-52_processed_v2.csv'
+             ]
 
-    x = Household(df, stoves, fuels)
-    print(
-         # x.check_stove_type(),
-        # x.check_fuel_type('lpg')
-        # x.cooking_events()
-        x.fuel_usage(),
-        # x.cooking_duration()
-        # x.df_stoves
-        # x.study_duration.total_seconds()/86400
-    )
+    for file in filepaths:
+        df, stoves, fuels, hh_id = reformat('./data_files/' + file)
+        x = Household(df, stoves, fuels, hh_id)
+        print(file, '\n',
+            # x.check_stove_type(),
+            # x.check_fuel_type('lpg')
+            # x.cooking_events()
+            x.fuel_usage() #, '\n',
+            # x.cooking_duration()
+            # x.df_stoves
+            # x.study_duration.total_seconds()/86400
+        )
+        x.plot_fuel(fuel_usage=True)
 
+    # df, stoves, fuels, hh_id = reformat('./data_files/HH_38_2018-08-26_15-01-40_processed_v3.csv')
+    # x = Household(df, stoves, fuels, hh_id)
+    # print(x.fuel_usage('firewood'))
     # x.plot_fuel(fuel_usage=True)
-    # x.plot_stove(cooking_events=True)
-#
+
